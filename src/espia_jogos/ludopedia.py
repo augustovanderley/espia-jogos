cac16b2b36dd3195cb7d4e7b61b082efbb0b6364
DyegoSoares <dyego_soares@mckinsey.com>
2024-04-29 23:52:58 -0300

    reorganizei api em classe isolada

---
+++ src/espia_jogos/ludopedia.py
@@ -0,0 +1,41 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv(".env")
+
+
+class Ludopedia:
+    base_url = "https://ludopedia.com.br/api/v1"
+    headers = {"Authorization": f"Bearer {os.getenv("LUDOPEDIA_ACCESS_TOKEN")}"}
+
+    @classmethod
+    def request_collection(cls, id_usuario, nome_jogo):
+        endpoint = f"{cls.base_url}/colecao"
+        params = {
+            "id_usuario": id_usuario,
+            "lista": "colecao",
+            "ordem": "nome",
+            "tp_jogo": "t",
+            "rows": 100,  # Número de resultados por página
+            "search": nome_jogo,
+        }
+        response = requests.get(endpoint, headers=cls.headers, params=params)
+        if response.status_code != 200:
+            print(f"Erro ao buscar dados: {response.status_code}")
+            return []
+        return response.json()["colecao"]
+
+    @classmethod
+    def request_users(cls, usuario_procurado):
+        endpoint = f"{cls.base_url}/usuarios"
+        params = {
+            "search": usuario_procurado,
+        }
+        response = requests.get(endpoint, headers=cls.headers, params=params)
+        if response.status_code != 200:
+            print(f"Erro ao buscar dados: {response.status_code}")
+            return []
+        return response.json()["usuarios"]
--- src/espia_jogos/main.py
+++ src/espia_jogos/main.py
@@ -1,11 +1,9 @@
-import requests
-import os
 import json
 
 import click
-from dotenv import load_dotenv
 
 from .usuarios import usuarios
+from .ludopedia import Ludopedia
 
 
 @click.group()
@@ -16,20 +14,18 @@ def main():
 @main.command()
 @click.argument("nome_jogo")
 def jogos(nome_jogo):
-    token = load_token()
 
     print(f"Quem tem o jogo com o termo {nome_jogo}?")
 
     for usuario in usuarios:
-        colecao = request_collection(usuario.id, nome_jogo, token)
+        colecao = Ludopedia.request_collection(usuario.id, nome_jogo)
         print_collection(usuario, colecao)
 
 
 @main.command()
 @click.argument("usuario_procurado")
 def usuario(usuario_procurado):
-    token = load_token()
-    usuarios = request_users(usuario_procurado, token)
+    usuarios = Ludopedia.request_users(usuario_procurado)
 
     for usuario in usuarios:
         if usuario["usuario"].lower() == usuario_procurado.lower():
@@ -37,21 +33,14 @@ def usuario(usuario_procurado):
             break
 
 
-def load_token():
-    load_dotenv(".env")
-    return os.getenv("LUDOPEDIA_ACCESS_TOKEN")
-
-
 def print_collection(usuario, colecao):
     if colecao:
-        print("-------------------------------------")
+        print("-" * 20)
         print(f"{usuario.nome_legivel}:")
         print("")
         for jogo in colecao:
             nota = jogo["vl_nota"]
-            if not nota:
-                nota = "-"
-            print(f"{jogo['nm_jogo']} {nota}")
+            print(f"{jogo['nm_jogo']} {nota if nota is not None else '-'}")
 
         print("")
 
@@ -63,43 +52,3 @@ def print_usuario(usuario):
         "nome_legivel": "INSIRA NOME LEGIVEL",
     }
     print(json.dumps(informacao, indent=4))
-
-
-def request_collection(id_usuario, nome_jogo, token):
-    BASE_URL = "https://ludopedia.com.br/api/v1/colecao"
-
-    token = load_token()
-    headers = {"Authorization": f"Bearer {token}"}
-
-    params = {
-        "id_usuario": id_usuario,
-        "lista": "colecao",
-        "ordem": "nome",
-        "tp_jogo": "t",
-        "rows": 100,  # Número de resultados por página
-        "search": nome_jogo,
-    }
-
-    response = requests.get(BASE_URL, headers=headers, params=params)
-    if response.status_code != 200:
-        print(f"Erro ao buscar dados: {response.status_code}")
-        return []
-
-    colecao = response.json()["colecao"]
-
-    return colecao
-
-
-def request_users(usuario_procurado, token):
-    token = load_token()
-    base_url = "https://ludopedia.com.br/api/v1/usuarios"
-    headers = {"Authorization": f"Bearer {token}"}
-    params = {
-        "search": usuario_procurado,
-    }
-
-    response = requests.get(base_url, headers=headers, params=params)
-    if response.status_code != 200:
-        print(f"Erro ao buscar dados: {response.status_code}")
-        return []
-    return response.json()["usuarios"]
