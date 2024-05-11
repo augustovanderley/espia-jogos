4e6999c800f4f9b8536bade8a1f5e09380b92c7d
Augusto <augustovanderley@gmail.com>
2024-05-11 14:48:27 -0300

    fix ruff

--- espia_jogos/front.py
+++ espia_jogos/front.py
@@ -2,12 +2,13 @@ import streamlit as st
 from espia_jogos.usuarios import usuarios
 from espia_jogos.ludopedia import Ludopedia
 
+
 def display_collection(usuario, colecao):
     if colecao:
         name_to_display = usuario.nome_legivel if usuario.nome_legivel else usuario.usuario
         thumbnail_url = usuario.thumb
         # Start an HTML table
-        html_content = f"<style>td, th {{text-align: left; padding: 8px;}}</style>"
+        html_content = "<style>td, th {{text-align: left; padding: 8px;}}</style>"
         html_content += "<table border='1'>"
         html_content += "<tr><th>Jogo</th><th>Nota</th></tr>"
         
